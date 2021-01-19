314c987af4a733b54b6f89afe2e55e86ad494dca
kimjinmi <jinmikim88@gmail.com>
2021-01-19 17:25:23 +0900

    create customer.js

--- ReactProject/management/src/App.js
+++ ReactProject/management/src/App.js
@@ -1,12 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
 
+const customers = [{
+  'id' : 1,
+  'image' : 'http://placeimg.com/200/200/1',
+  'name' : '홍길동',
+  'birthday' : '961222',
+  'gender' : '남',
+  'job' : '대학생'
+},
+{
+  'id' : 2,
+  'image' : 'http://placeimg.com/200/200/2',
+  'name' : '이순신',
+  'birthday' : '961112',
+  'gender' : '남',
+  'job' : '디자이너'
+},
+{
+  'id' : 3,
+  'image' : 'http://placeimg.com/200/200/3',
+  'name' : '가나다',
+  'birthday' : '960203',
+  'gender' : '남',
+  'job' : '대학생'
+}]
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's develop management system!</h2>
-    </div>
+   <div>
+     {
+       customers.map(c =>{
+         return(
+           <Customer
+           key={c.id}
+           id={c.id}
+           image={c.image}
+           name={c.name}
+           birthday={c.birthday}
+           gender={c.gender}
+           job={c.job}/>
+         )
+       })
+     }
+     
+    </div>  
   );
 }
 
